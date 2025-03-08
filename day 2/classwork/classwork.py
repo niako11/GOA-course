d13c2d9187e08c8933df22972abc87f8d8a70ced
Anamarii129 <anamariakabulashvili32@gmail.com>
2025-03-09 03:22:42 +0400

    3 commit

---
+++ day 2/classwork/classwork.py
@@ -0,0 +1,10 @@
+name = "niako"
+surmane = "kabulahvili"
+age = "13"
+favorite_color = "vardisferi"
+print(name)
+print(surmane)
+print(age)
+print(favorite_color)
+
+
---
+++ day 2/homework/homework2.py
@@ -0,0 +1,12 @@
+name = "nia kabulahvuli "
+print(name[0] + "." + name[4])
+name = "anamaria kabulahvili"
+print(name [0] + "."+ name [9])
+name = "luka kabilahvili"
+print(name [0] +"."+ name [5])
+name ="giorgi kabulahvili"
+print(name [0]  +"." + name [7])
+name ="tamuna chinchalaze"
+print(name [0] + "." + name [7])
+
+
