b6eb9752ac3dea934f36b5a5df11392caf30fa0a
Anamarii129 <anamariakabulashvili32@gmail.com>
2025-03-21 07:20:16 +0400

    3

---
+++ day 6/classwork/classwork2.py
@@ -0,0 +1,13 @@
+fav_color =input("dawere heni sayvareli feri:")
+print("chemi sayareli feri aris "+" "+fav_color)
+
+fav_animal = input("dawere heni sayvareli cxoveli:")
+print("chemi sayvareli cxoveli aris "+" "+fav_animal)
+
+fav_film = input("dawere heni sayvareli filmi:")
+print("chemi sayvareli filmi aris "+" "+fav_film )
+
+age = int(input("dawere heni asaki:"))
+print("me var " + str(age) + "wlis")
+
+
---
+++ day 6/homework/homework1.py
@@ -0,0 +1,8 @@
+name = input("dawere heni saxeli da gvari:")
+age = input("dawere heni asaki:")
+live = input("sad cxovrob:")
+print("me var "+" "+name + " " + " me var " + age + " wlis " + " me vcxovrob " + live )
+
+
+
+
