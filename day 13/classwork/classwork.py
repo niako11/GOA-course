809420d97eb168f87c72efb67df50387f1eac5a9
Anamarii129 <anamariakabulashvili32@gmail.com>
2025-04-15 04:42:31 +0400

    3

---
+++ day 13/classwork/classwork.py
@@ -0,0 +1,11 @@
+  
+#გამოიცანი რომელი ტექნიკის მაგალითებია ქვემოთ მოცემული მაგალითები (გამეორების , არჩევის ,თუ თანმიმდევრობის
+
+print("Hello World")#თანმიმდრეობა
+print("Niako")
+print("Anaaia")
+
+
+print(True and False)#არჩევა არას
+print(False or True)#კის
+print(bool(1) and bool(0) )#არას
---
+++ day 13/homework/hemajamebeli1.py
@@ -0,0 +1,49 @@
+#აღწერე ყველა ქვემოთ ჩამოთვლილი ფუნქცია
+print() #gamotana
+str() #"niako"-
+int() # 12-
+bool() # False and True-
+float() #1.34-
+type() #-
+
+#ქვემოთ ჩამოთვლილთაგან რომელი არის კარგი #პრაქტიკა და რომელი არის ცუდი პრაქტიკა #მიუწერე კომენტარების სახით
+
+#2myname = "niako"#cudi pratika+
+MYAGE = 13#cudi pratika+
+my_name = "Niako"#kargi pratika+
+_favAnimals = "Dog"#kargi pratika-
+FAVoriTE-color = "Red"#cudi pratika+
+#is_programer = #kargi pratika +
+is_beautiful = True#kagi pratika+
+
+#იპოვე შეცდომები და გაასწორებული ვერსია #მიუწერე გვერდით კომენტარების სახით
+#name = niako#"niako"+
+#age-13#age=13+
+#my_sister(anamaria)#my_sister="anamaria"+
+#2animal_"Monkey #animal="monkey"+
+#is_programer - True#is_programer=True+
+#is beautiful (Trrue)#is_beautiful= true+
+
+#აღწერე შემდეგი ტერმინები
+#Concatenation-შეკრება/შერთება(მაგ სტრინგებისშერთება)+
+#iteration-გამეორება(ციკლების)+
+#sequence-თამინდრეობა+
+#selection-არჩევა+
+#incrementation-გარზდა+
+#decrementation-შემცირება+
+
+#აღწერე შედეგი მონაცემთა ტიპები
+#string-სიტყვას ჭირდება ბჭწალები+
+#integer-ეს არის მთელი რიცვი+
+#bolean-False and Tru+
+#float-ართწილადი+
+
+#კომენტაების გამოყენებით გვერით მიუწერე  რას #გამოიტანს კოდი
+print(True and False)#არას+
+print(False and False)#არას+
+
+bool1 = True
+bool2 = False
+
+print(bool1 and bool2)#არას+
+print(bool2 or bool1)#კის+
