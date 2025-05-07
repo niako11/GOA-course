3981f3db3b432f33ec628ef509647250183f8b6c
Anamarii129 <anamariakabulashvili32@gmail.com>
2025-05-07 08:19:43 +0400

    3

---
+++ day 18/classwork/claswork.py
@@ -0,0 +1,18 @@
+#1)გადაიტანე შენს ფაილში და მიუწერე გვერდით სწორი პასუხები კომენტარების გამოყენებით
+
+#1)
+is_student = True
+age = 19
+print(is_student and age>18) # აქ დაუწერე პასუხი...True
+
+#2)
+number1 = 30
+number2 = 54
+
+print(number1<number2 and number1>number2) # აქ დაუწერე პასუხი...Fasle
+
+#3)
+print( (20>30) or (22!=10)  ) # აქ დაუწერე პასუხი...True
+
+#4)
+print( (22 == 10) or (30 != 30 ) )# აქ დაუწერე პასუხი...Fasle
---
+++ day 18/homework/homework2.py
@@ -0,0 +1,10 @@
+# ***Day 18 Homework:***
+
+#2)შექმენი ცვლადი სადაც შეინახავ შენს სახელს ,  შემდეგ if ის გამოყენებით შეამოწმე თუ ეს სახელი ტოლია "Nia"-ანუ შენი სახელის , მაშინ 
+# დაპრინტოს "Hello "+name , ხოლო სხვა შემთხვევაში დაპრინტოს "Hello Guest"
+name  = "giorgi"
+if name == "niako":
+    print("Hello"+name)
+else:
+    print("Hello Guest")
+
