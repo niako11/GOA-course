bad942108ad051b45f8061859c00cb1a19d78bf3
Anamarii129 <anamariakabulashvili32@gmail.com>
2025-05-12 08:09:21 +0400

    3

---
+++ day 20/homework/Semajamebeli2.py
@@ -0,0 +1,95 @@
+#1)გადაიტანე შენს ფაილში და მიუწერე გვერდით სწორი პასუხები კომენტარების გამოყენებით
+
+is_student = True
+age = 19
+print(is_student and age>18) # აქ დაუწერე პასუხი...True
+
+number1 = 30
+number2 = 54
+
+print(number1<number2 and number1>number2) # აქ დაუწერე პასუხი...fasle
+
+print( (20>30) or (22!=10)  ) # აქ დაუწერე პასუხი...true
+
+print  (22 == 10) or (30 != 30 ) # აქ დაუწერე პასუხი...fasle
+
+
+#2)ყველა ლოგიკურ ოპეაციას მიუწერე საბოლოო შედეგი კომენტარების გამოყენებით
+
+print(True and False)#fasle
+print(True and True)#true
+print(False and False)#fasle
+print(False and True)#fasle
+print(False or True)#true
+print(True or False)#true
+print(False or False)#fasle
+print(True or True)#true
+print(False or True)#true
+print(False and True)#fasle
+
+
+#3) თითოეულ ელემენტს მიუწერე თავისი ტიპი კომენტარების გამოყენებით
+
+#"niako"-string
+#20-intejer
+#1.5-floa
+#"Giorgi"-string
+#10.25-floa
+#"anamaria"-string
+#27-intejer
+
+
+#4)გააკეთე string-ების concatenation(კონკატენაცია, შეერთება)ისე რომ შედეგი მიიღო : "niako kabulashvili
+
+word1 = "ni"
+word2 = "a"
+word3 = "ko"
+surname1 = "kabula"
+surname2 ="shvili"
+print(word1 + word2+word3)
+print(surname1+surname2)
+
+#5)იპოვე შეცდომები  და გადაასწორე სწორ ვარიანტებად
+#name = "niako"
+#age= 13
+#surname = kabulashvili
+#print ( name +" "+surname)
+#my_sister = "anamaria kabulashvili"
+
+
+#6)რომელია კარგი და ცუდი პრაქტიკა კომენტარების გამოყენებით მიუწერე ყველას გვერდით , ასევე შეასწორე შეცდომები
+#2NAME = "Niako"-ცუდი
+myAGE = 13#-ცუდი
+myFAVORITEAnimal = "Monkey"#-ცუდი
+surname = "kabulashvili"#კარგი
+my_fav_food = "Khinkali"#კარგი
+#25street = "Gldani"ცუდი
+dogName = "Goofy"#ცუდი
+DogNAME = "Goofy"#ცუდი
+DOGNAME5 = "Goofy"#ცუდი
+dog_name = "Goofy"#კარგი
+name = 10#ცუდი
+surname = "Niako"#ცუდი
+name = "kabulashvili"#ცუდი
+fav_animal = "green"#ცუდი
+#print = "Hello#ცუდი
+#my_name= "Niako"კარგი
+#color = 
+
+#7)აღწერე
+print() #გამოაქვს შეგედი ტერმინაში
+str() #მონაცემებს გადაექცევს  სტრინგებად
+int() # მონაცემებს გადაქცევს მთელ რიცხვებად 
+bool() # მონაცემებს გადაქცევს True Fasle
+float() #მონაცემებს გადაქცევს ათწილადად 1.77
+type() #იგებს მონაცემთან ტიპეპს
+
+#8)ქვემოთ ჩამოთვლილთაგან რომელი არის კარგი #პრაქტიკა და რომელი არის ცუდი პრაქტიკა #მიუწერე კომენტარების სახით
+
+#2myname = "niako"cudi
+MYAGE = 13#cudi
+my_name = "Niako"#kargi
+_favAnimals = "Dog"#kargi
+FAVoriTE-color = "Red"#cudi
+is_programer = True#kargi
+is_beautiful = True#cudi
---
+++ day 20/homework/homework 4.py
@@ -0,0 +1,7 @@
+name = "nikolozi"
+if name == "niako":
+    print("Hello"+name)
+elif name == "anamaria":
+    print("Hello"+name)
+else:
+    print("Hello guest")
