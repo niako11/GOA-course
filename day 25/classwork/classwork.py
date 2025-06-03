009b498f4480f5161e88d8153dc4ebfb7becd40d
niako11 <niakabulashvili032@gmail.com>
2025-06-03 04:13:52 +0400

    3

---
+++ day 25/classwork/classwork.py
@@ -0,0 +1,8 @@
+#1)რას გამოიტანს შემდეგი კოდი , შედეგი მიუწერე გვერდით კომენტარის სახით
+names = ["Nia","Anamaria","Luka","Giorgi"]
+print(names[:3])# nia anamaria luka 
+print(names[1:])#anamaria luka giorgi
+print(names[2:3])#luka 
+print(names[:])#nia anamaria luka giorgi
+print(names[3:])#giorgi
+print(names[0:4])#nia anamaria luka giorgi
